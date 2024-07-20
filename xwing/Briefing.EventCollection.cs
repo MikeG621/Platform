e95bdbd9534e815eb8c5372d70ce2a67558e876a
Mike <mjgaisser@gmail.com>
2024-07-20 12:42:45 -0500

    added Event.Clone

--- xwing/Briefing.EventCollection.cs
+++ xwing/Briefing.EventCollection.cs
@@ -256,6 +256,10 @@ namespace Idmr.Platform.Xwing
 				for (int i = 2; i < Length; i++) array[i] = Variables[i - 2];
 				return array;
 			}
+
+			/// <summary>Gets a copy of the event.</summary>
+			/// <returns>A new copy of the event.</returns>
+			public Event Clone() => new Event(GetArray());
 		}
 	}
 }
